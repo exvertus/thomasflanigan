cc3fe6d082095c93d9dc3eddf0830fdd5315a3c4
tflanigan <tflanig1@gmail.com>
2022-05-16 17:08:30 -0500

    Added multiple hooks for debugging and testing

--- plugins/directory-index/pelican/plugins/directory_index/directory_index.py
+++ plugins/directory-index/pelican/plugins/directory_index/directory_index.py
@@ -4,8 +4,30 @@ from pelican import signals
 
 log = logging.getLogger(__name__)
 
-def sanity_check(sender):
-    print('sanity check passed')
+def page_generator_context_hook():
+    log.info('break here')
+
+def page_generator_preread_hook():
+    log.info('break here')
+
+def page_generator_init_hook():
+    log.info('break here')
+
+def page_generator_finalized_hook():
+    log.info('break here')
+
+def page_generator_write_page_hook():
+    log.info('break here')
+
+def page_writer_finalized_hook():
+    log.info('break here')
+
 
 def register():
-    signals.initialized.connect(sanity_check)
+    signals.page_generator_context.connect(page_generator_context_hook)
+    signals.page_generator_preread.connect(page_generator_preread_hook)
+    signals.page_generator_init.connect(page_generator_init_hook)
+    signals.page_generator_finalized.connect(page_generator_finalized_hook)
+    signals.page_generator_write_page.connect(page_generator_write_page_hook)
+    signals.page_writer_finalized.connect(page_writer_finalized_hook)
+    
