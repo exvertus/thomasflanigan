1eeae33c6209102db4b7e1103052218fb17ea8dd
tflanigan <tflanig1@gmail.com>
2022-05-17 18:59:14 -0500

    Settings cleanup

--- pelicanconf.py
+++ pelicanconf.py
@@ -6,7 +6,6 @@ SITEURL = 'https://thomasflanigan.com'
 PATH = 'content'
 PAGE_PATHS = ['art', 'music', 'tech']
 ARTICLE_PATHS = ['tech/blog', 'music/poems-lyrics']
-ARTICLE_EXCLUDES = ['art/index.*', 'music/index.*', 'tech/index.*']
 PAGE_EXCLUDES = ['art/index.*', 'music/index.*', 'tech/index.*']
 OUTPUT_PATH = 'output/'
 PATH_METADATA = '(?P<path_no_ext>.*)\..*'
@@ -17,7 +16,7 @@ PLUGIN_PATHS = ['plugins\directory-index\pelican\plugins\directory_index']
 STATIC_PATHS = ['images']
 
 TIMEZONE = 'America/Chicago'
-DEFAULT_LANG = 'English'
+DEFAULT_LANG = 'en'
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -26,12 +25,6 @@ TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
-# Blogroll
-# LINKS = (('Art', '/art'),
-#          ('Music', '/music'),
-#          ('Tech', '/tech'),)
-
-# Social widget
 SOCIAL = (('Instagram', 'https://www.instagram.com/tomisdrawingagain/'),
           ('Bandcamp', 'https://materialsoul.bandcamp.com/'),
           ('Github', 'https://github.com/exvertus/'),
@@ -45,15 +38,12 @@ DEFAULT_DATE_FORMAT = '%d %b %Y'
 
 DISPLAY_PAGES_ON_MENU = False
 DISPLAY_CATEGORIES_ON_MENU = False
-DIRECT_TEMPLATES = ['index', 'categories', 'tags', 'archives']
+DIRECT_TEMPLATES = ['categories', 'tags', 'archives']
 
-# YEAR_ARCHIVE_SAVE_AS = 'posts/{date:%Y}/index.html'
 LOAD_CONTENT_CACHE = False
 DELETE_OUTPUT_DIRECTORY = True
 
 USE_FOLDER_AS_CATEGORY = True
-
-AUTHOR_SAVE_AS = 'author/{slug}.html'
 
 # Local-only settings ---
 RELATIVE_URLS = True
