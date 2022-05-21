ca73a761a5b11172d3613c1f5dff6a37a5dce158
tflanigan <tflanig1@gmail.com>
2022-05-20 19:12:51 -0500

    Generate context function added

--- plugins/directory-index/pelican/plugins/directory_index/directory_index.py
+++ plugins/directory-index/pelican/plugins/directory_index/directory_index.py
@@ -1,7 +1,9 @@
 import logging
 import os
+from pathlib import Path
 
 from pelican import generators, signals
+from pelican.contents import Content
 from pelican.readers import Readers
 from pelican.utils import DateFormatter
 
@@ -9,9 +11,23 @@ from jinja2 import ChoiceLoader, Environment, FileSystemLoader, PrefixLoader
 
 log = logging.getLogger(__name__)
 
-class IndexGenerator:
+class IndexPage(Content):
+    mandatory_properties = tuple()
+    allowed_statuses = ('published')
+    default_status = 'published'
+    default_template = 'page'
+
+    def _expand_settings(self, key):
+        klass = 'draft_page' if self.status == 'draft' else None
+        return super()._expand_settings(key, klass)
+
+class IndexGenerator(generators.Generator):
+    # TODO: Add caching by importing CachingGenerator
     def __init__(self, context, settings, path, theme, output_path,
                  readers_cache_name='', **kwargs):
+        # Do not use super init from baseclass generator unless tested... 
+        # risks over-sending generator_init signals... 
+        # other base methods do not send signals
         self.context = context
         self.settings = settings
         self.path = path
@@ -65,15 +81,43 @@ class IndexGenerator:
         custom_tests = self.settings['JINJA_TESTS']
         self.env.tests.update(custom_tests)
 
-        self.file_stem = self.settings.get("DIRECTORY_INDEX_STEM")
-        log.info('break here')
+        self.file_stem = self.settings.get("DIRECTORY_INDEX_STEM", 'index')
+        self.settings['INDEXPAGE_URL'] = self.settings.get("INDEXPAGE_URL", '{path_no_ext}.html')
+        self.settings['INDEXPAGE_SAVE_AS'] = self.settings.get("INDEXPAGE_SAVE_AS", '{path_no_ext}.html')
+        self.index_pages = {}
 
     def generate_context(self):
         """
         Find all index.{ext} files in content folder
-        and add them to self.pages
+        Add them to self.context['generated_content'] 
+        Careful: context dict ref shared globally between generators
         """
-        log.info('break here')
+        for ext in self.readers.extensions:
+            glob_pattern = f"**/*{self.settings['DIRECTORY_INDEX_STEM']}.{ext}"
+            ext_indexes = Path(self.settings['PATH']).glob(glob_pattern)
+            for index_path in ext_indexes:
+                try:
+                    index_page = self.readers.read_file(
+                        base_path=self.path, 
+                        path=index_path, 
+                        content_class=IndexPage,
+                        context=self.index_pages,
+                        preread_signal=None,
+                        preread_sender=self,
+                        context_signal=None,
+                        context_sender=self)
+                except Exception as e:
+                    log.error(
+                        'Could not process %s\n%s', index_path, e,
+                        exc_info=self.settings.get('DEBUG', False))
+                    self._add_failed_source_path(index_path)
+                    continue
+
+                if not index_page.is_valid():
+                    self._add_failed_source_path(index_path)
+                    continue
+
+                self.add_source_path(index_page)
 
     def generate_output(self, writer):
         log.info('break here')
