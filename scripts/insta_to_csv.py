13aada5d8a1cd5b498ae50496dc867c71ecada06
tflanigan <tflanig1@gmail.com>
2022-06-17 23:04:14 -0500

    Updated script to generating soup

--- scripts/insta_to_csv.py
+++ scripts/insta_to_csv.py
@@ -7,8 +7,9 @@ log = logging.getLogger(__name__)
 
 def parse_followers(read_filepath):
     log.info(f"Parsing followers from ${read_filepath}...")
-    with open(read_filepath) as rf:
-        soup = bs4.BeautifulSoup(rf, 'lxml')
+    raw = open(read_filepath, 'rb').read()
+    cleaned = bs4.UnicodeDammit.detwingle(raw)
+    soup = bs4.BeautifulSoup(cleaned, 'xml')
     log.debug('stop here')
     return followers
 
@@ -19,8 +20,8 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description='Convert scraped instagram data to CSV file.')
     parser.add_argument('data', help='path to readable file')
-    parser.add_argument('target', required=False, 
-        help='path to write CSV output to', default='./output.csv')
+    parser.add_argument('target', 
+        help='path to write CSV output to', default='../build/output.csv')
     args = parser.parse_args()
-    followers = parse_followers(pathlib.Path(args.data))
-    followers_to_csv(followers, pathlib.Path(args.target))
+    followers = parse_followers(pathlib.Path(args.data).expanduser())
+    followers_to_csv(followers, pathlib.Path(args.target).expanduser())
