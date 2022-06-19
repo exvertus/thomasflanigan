c870b7152a928b4c450d61e3ab214484498495df
tflanigan <tflanig1@gmail.com>
2022-06-18 23:22:32 -0500

    Add links

--- pelicanconf.py
+++ pelicanconf.py
@@ -39,10 +39,16 @@ TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
-SOCIAL = (('Instagram', 'https://www.instagram.com/material_soul/'),
-          ('Bandcamp', 'https://materialsoul.bandcamp.com/'),
-          ('Github', 'https://github.com/exvertus/'),
-          ('LinkedIn','https://www.linkedin.com/in/thomas-flanigan/'),)
+QUICKLINKS = (
+    ('Instagram', 'https://www.instagram.com/material_soul/'),
+    ('Etsy', 'https://www.etsy.com/shop/MaterialSoulArt'),
+    ('Deviant Art', 'https://www.deviantart.com/material-soul'),
+    ('Twitter', 'https://twitter.com/Material__Soul'),
+    ('Tumblr', 'https://www.tumblr.com/blog/material-soul'),
+    ('Facebook', 'https://www.facebook.com/materialsoul'),
+    ('Bandcamp', 'https://materialsoul.bandcamp.com/'),
+    ('Github', 'https://github.com/exvertus/'),
+    ('LinkedIn','https://www.linkedin.com/in/thomas-flanigan/'),)
 
 DEFAULT_PAGINATION = False
 DEFAULT_DATE_FORMAT = '%d %b %Y'
