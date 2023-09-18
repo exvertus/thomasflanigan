b4386b2e6d4b257c8138b8fc73518c9ebae54a23
Tom (cloud shell) <tflanig1@gmail.com>
2023-09-18 02:55:48 +0000

    fixes

--- test-build.sh
+++ test-build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 gcloud builds submit --region=us-central1 --project=$GOOGLE_CLOUD_PROJECT --config=cloudbuild.yaml \
-  --substitutions=_IMAGE="thomasflanigan",_LOCATION="us-central1-c",_CLUSTER="maincluster",_AR_PATH="us-central1-docker.pkg.dev"
+  --substitutions=_IMAGE="thomasflanigan",_AR_PATH="us-central1-docker.pkg.dev"
